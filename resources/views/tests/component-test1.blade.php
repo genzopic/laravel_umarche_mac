19ecdab4664a28fff1a1d4546d1ba73d9aac3723
genzo <genzo.pic@gmail.com>
2022-02-10 20:50:49 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -5,4 +5,5 @@
     コンポーネントテスト１
     <x-tests.card title="タイトル１" content="本文１" :message="$message" />
     <x-tests.card title="タイトル２" />
+    <x-tests.card title="cssを変更したい" class="bg-red-300" />
 </x-tests.app>
