4161f882656035625fd45467a8b0136393285814
genzo <genzo.pic@gmail.com>
2022-02-11 11:30:36 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,5 +3,8 @@
         ヘッダー２
     </x-slot>
     コンポーネントテスト２
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="クラスでのメッセージです" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="クラスでのメッセージです" defaultMessage="初期値から変更しています" />
 </x-tests.app>
-    
