1e6cab49e640093891a89e70c124eac1345e34d4
genzo <genzo.pic@gmail.com>
2022-02-18 00:02:35 +0900

    index, create

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -21,16 +21,15 @@ class OwnersController extends Controller
     public function index()
     {
         // カーボンテスト
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
-        echo $date_now->year;
-        echo $date_parse;
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
+        // echo $date_now->year;
+        // echo $date_parse;
 
         // エロクアント
-        $e_all = Owner::all();
-
+        // $e_all = Owner::all();
         // クエリビルダー
-        $q_get = DB::table('owners')->select('name','created_at')->get();
+        // $q_get = DB::table('owners')->select('name','created_at')->get();
         // $q_first = DB::table('owners')->select('name','created_at')->first();
 
         // コレクション
@@ -41,9 +40,12 @@ class OwnersController extends Controller
         // var_dump($q_first);
         //
         // dd($e_all,$q_get,$q_first,$c_test);
+
+        $owners = Owner::select('name','email','created_at')->get();
+
         return view('admin.owners.index',
-        compact('e_all','q_get'));
-        
+        compact('owners'));
+
     }
 
     /**
@@ -54,6 +56,7 @@ class OwnersController extends Controller
     public function create()
     {
         //
+        return view('admin.owners.create');
     }
 
     /**
