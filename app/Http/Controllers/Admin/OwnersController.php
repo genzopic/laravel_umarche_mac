74e8bd2f5f8c9b359d33a6f0f63ce08108be0055
genzo <genzo.pic@gmail.com>
2022-02-19 00:13:50 +0900

    pagination

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -44,7 +44,9 @@ class OwnersController extends Controller
         // dd($e_all,$q_get,$q_first,$c_test);
 
         // 一覧画面
-        $owners = Owner::select('id','name','email','created_at')->get();
+        // $owners = Owner::select('id','name','email','created_at')->get();
+        // ページネーション対応
+        $owners = Owner::select('id','name','email','created_at')->paginate(3);
 
         return view('admin.owners.index',compact('owners'));
 
