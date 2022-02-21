a5bc1def3a6134a13e656ceb3b6a0b194ceff574
genzo <genzo.pic@gmail.com>
2022-02-22 00:36:31 +0900

    middleware, abort(404)

---
+++ app/Http/Controllers/Owner/ShopController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers\Owner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+//
+use Illuminate\Support\Facades\Auth;        // ログインユーザー
+use App\Models\Shop;                        // shopモデル
+use Illuminate\Support\Facades\DB;          // QueryBuilder クエリビルダー
+
+class ShopController extends Controller
+{
+    // コンストラクタ
+    public function __construct()
+    {
+        // 認証チェック
+        $this->middleware('auth:owners');
+        // 
+        $this->middleware(function(Request $request,$next) {
+            // dd($request->route()->parameter('shop'));   // 文字列
+            // dd(Auth::id()); // 数字
+
+            $id = $request->route()->parameter('shop'); // shopのidを取得
+            if(!is_null($id)){
+                // owner/shops/indexにアクセスするとnullになるので
+                // それ以外の場合（owner/shops/edit/{shop}）
+                $shopsOwnerId = Shop::findOrFail($id)->owner->id;
+                $ownerId = Auth::id();
+                if($shopsOwnerId !== $ownerId) {
+                    // ログインしたオーナーと違う場合は、404
+                    abort(404);     // 404画面表示
+                }
+            }
+
+            return $next($request);
+        });
+
+    }
+    // 一覧
+    public function index()
+    {
+        $owner_id = Auth::id();
+        $shops = Shop::where('owner_id',$owner_id)->get();
+
+        return view('owner.shops.index',compact('shops'));
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        dd(Shop::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        
+    }
+ 
+
+}
