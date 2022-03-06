8efe32069b376beec0f60b34aa3eace6dc05bcdf
genzo <genzo.pic@gmail.com>
2022-03-06 20:16:46 +0900

    表示順

--- app/Constants/Common.php
+++ app/Constants/Common.php
@@ -3,11 +3,24 @@ namespace App\Constants;
 
 class Common
 {
+    // 在庫の入出庫タイプ
     const PRODUCT_ADD = '1';
     const PRODUCT_REDUCE = '2';
     const PRODUCT_LIST = [
         'add' => self::PRODUCT_ADD,
         'reduce' => self::PRODUCT_REDUCE
     ];
-
+    // 表示順
+    const ORDER_RECOMMEND = '0'; 
+    const ORDER_HIGHER = '1'; 
+    const ORDER_LOWER = '2';
+    const ORDER_LATER = '3';
+    const ORDER_OLDER = '4';
+    const SORT_ORDER = [
+        'recommend' => self::ORDER_RECOMMEND,
+        'higherPrice' => self::ORDER_HIGHER,
+        'lowerPrice' => self::ORDER_LOWER,
+        'later' => self::ORDER_LATER,
+        'older' => self::ORDER_OLDER
+    ];
 }
