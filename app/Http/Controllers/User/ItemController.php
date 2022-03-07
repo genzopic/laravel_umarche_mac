0bf32c5f3319e71ecb4da8de3cc5e3ec7cbac84c
genzo <genzo.pic@gmail.com>
2022-03-07 12:19:22 +0900

    検索フォームカテゴリー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 //
 use App\Models\Product;
 use App\Models\Stock;
+use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
@@ -36,10 +37,15 @@ class ItemController extends Controller
     //
     public function index(Request $request)
     {
+        // dd($request);
         $products = Product::availableItems()
+                    ->selectCategory($request->category ?? '0')
                     ->sortOrder($request->sort)
                     ->paginate($request->pagination ?? '20');
-        return view('user.index',compact('products'));
+        $categories = PrimaryCategory::with('secondary')
+                    ->get();
+    
+        return view('user.index',compact('products','categories'));
     }
     //
     public function show($id)
