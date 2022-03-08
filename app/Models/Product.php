c1af2d2a3e4385dfeb349c120f4e87d435e27f14
genzo <genzo.pic@gmail.com>
2022-03-08 09:09:04 +0900

    検索フォームキーワード

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -69,7 +69,7 @@ class Product extends Model
         return $this->belongsToMany(Product::class,'carts')
                     ->withPivot(['id','quantity']);
     }
-
+    // 有効な商品（在庫があり、販売中）
     public function scopeAvailableItems($query)
     {
         $stocks = DB::table('t_stocks')
@@ -97,7 +97,7 @@ class Product extends Model
                         )
         ;
     }
-
+    // 並び順
     public function scopeSortOrder($query,$sortOrder)
     {
         if($sortOrder === null || $sortOrder === \Constant::SORT_ORDER['recommend']){
@@ -116,11 +116,28 @@ class Product extends Model
             return $query->orderBy('products.created_at', 'asc') ;
         }
     }
-
+    // カテゴリー検索
     public function scopeSelectCategory($query,$categoryId)
     {
         if($categoryId !== '0'){
             return $query->where('secondary_category_id',$categoryId);
+        } else {
+            return ;
+        }
+    }
+    // キーワード
+    public function scopeSearchKeyword($query,$keyword)
+    {
+        if(!is_null($keyword)){
+            //全角スペースを半角に
+            $spaceConvert = mb_convert_kana($keyword,'s'); 
+            //空白で区切る
+            $keywords = preg_split('/[\s]+/', $spaceConvert,-1,PREG_SPLIT_NO_EMPTY); 
+            //単語をループで回す
+            foreach($keywords as $word) {
+                $query->where('products.name','like','%'.$word.'%');
+            }
+            return $query;
         } else {
             return ;
         }
